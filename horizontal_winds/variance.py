7a4e98650c1bef8f11067571e99498db48faa076
Swinda Falkena <skf1018@imperial.ac.uk>
2019-03-19 11:37:57 +0000

    Variance computation.

---
+++ horizontal_winds/variance.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import statistics as stat
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x_dat = pd.read_csv("velocity_x.csv")
+y_dat = pd.read_csv("velocity_y.csv")
+
+time_nr = len(x_dat)
+xdat = np.zeros((time_nr,9,9))
+ydat = np.zeros((time_nr,9,9))
+for i in range(0,time_nr):
+    xdat[i] = np.array( x_dat.iloc[i][1::] ).reshape(9,9)
+    ydat[i] = np.array( y_dat.iloc[i][1::] ).reshape(9,9)
+
+x_av = np.average( xdat, axis=0 )
+y_av = np.average( ydat, axis=0 )
+
+x_var = np.var( xdat, axis=0 )
+y_var = np.var( ydat, axis=0 )
+
+x_cov = np.cov( xdat.reshape(time_nr,81), rowvar=False )
+y_cov = np.cov( np.transpose( ydat, axes=(0,2,1) ).reshape(time_nr,81), rowvar=False  )
+
+# plt.contourf( x_cov )
+# plt.colorbar()
+# plt.savefig('plots/cov_x')
+# plt.show()
+
+plt.contourf( y_cov )
+plt.colorbar()
+# plt.savefig('plots/cov_y')
+plt.show()
+
+### Plot variance in contours ###
+
+# cbar_max = 0.002
+# cbar_min = 0
+# cbar_step = 0.00025
+# cbar_nr = int( cbar_max /cbar_step)
+# cbar_tick = np.arange( cbar_min, cbar_max+cbar_step, cbar_step )
+#
+# plt.contourf(x_var, cbar_tick, cmap = plt.cm.get_cmap('viridis',cbar_nr), vmin=cbar_min, vmax=cbar_max)
+# plt.title( 'Variance in x direction' )
+# plt.colorbar( ticks=cbar_tick )
+# plt.savefig("plots/variance_x.png")
+# plt.show()
+#
+# plt.contourf(y_var, cbar_tick, cmap = plt.cm.get_cmap('viridis',cbar_nr), vmin=cbar_min, vmax=cbar_max)
+# plt.title( 'Variance in y direction')
+# plt.colorbar(ticks=cbar_tick )
+# plt.savefig("plots/variance_y.png")
+# plt.show()
